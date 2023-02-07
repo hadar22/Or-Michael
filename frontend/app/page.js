bd28a4a86e3b243f1ee412d6069cf83c504ec82e
Daniel <daniel3levi@gmail.com>
2023-02-07 15:40:50 +0200

    Update routs

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
+import BulletinBoard from '../components/BulletinBoard';
+
 export default function Home() {
   return (
     <div className="w-full h-screen homepage">
       <div className="w-4/5 text-center absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2">
         <h1 className="text-red-400 text-3xl">לוח התנדבויות</h1>
-        
+        <BulletinBoard props={ads} />
       </div>
     </div>
   );
