9973b73bfcce14e33b514672fef9bb5e79c068e1
gateniomer <gateniomer@gmail.com>
2023-02-15 22:23:42 +0200

    added filter component

--- frontend/components/BulletinBoard.jsx
+++ frontend/components/BulletinBoard.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-
-import VolunteerAd from '../components/VolunteerAd';
+import React from "react";
+import FilterBar from "./FilterBar";
+import VolunteerAd from "../components/VolunteerAd";
 
 const BulletinBoard = ({ volunteer_ads }) => {
   const renderList = volunteer_ads.map((ad) => (
     // console.log(item.id)
     <VolunteerAd key={ad._id} ad={ad} />
   ));
+
+  //Will be api endpoint for hospitals
+  const hospitals = volunteer_ads.map((ad) => ad.hospital);
+
   return (
     <>
       <div>
-        <div>Searchbar component</div>
+        <FilterBar data={hospitals} />
         <div>
           <div className="grid md:grid-cols-3  gap-4 absolute">
             {renderList}
